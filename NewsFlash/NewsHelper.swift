33c99c4eecb6e84b356a72d7b3d27f54242fab90
MAwais <mawais54013@outlook.com>
2019-12-07 15:08:07 -0800

    Fixing Alamofire issue

--- NewsFlash/NewsHelper.swift
+++ NewsFlash/NewsHelper.swift
@@ -11,9 +11,26 @@ import Alamofire
 class NewsHelper {
     func getArticles() {
         
-        AF.request("Module 'Alamofire' has no member named 'request'").response { (response) in
+        AF.request("https://newsapi.org/v2/top-headlines?country=us&apiKey=88a51528f6614bcd82f3d8ea3c68cce6").responseJSON { (response) in
             print(response)
+            
+            if{
+                
+                let weatherJSON : JSON = JSON(response.result.value!)
+                
+            }
+            else
+            {
+                print("Error \(response.result.error)")
+            }
         }
-        
     }
+}
+
+class Article {
+    var title = ""
+    var urlToImage = ""
+    var url = ""
+    var description = ""
+    var category = ""
 }
