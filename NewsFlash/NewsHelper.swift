e5266e7fe258fc9db9c685ca11f1238cb752db7b
MAwais <mawais54013@outlook.com>
2019-12-07 14:39:36 -0800

    Added New Controllers

---
+++ NewsFlash/NewsHelper.swift
@@ -0,0 +1,19 @@
+//
+//  NewsHelper.swift
+//  
+//
+//  Created by muhammad Awais on 12/7/19.
+//
+
+import Foundation
+import Alamofire
+
+class NewsHelper {
+    func getArticles() {
+        
+        AF.request("Module 'Alamofire' has no member named 'request'").response { (response) in
+            print(response)
+        }
+        
+    }
+}
