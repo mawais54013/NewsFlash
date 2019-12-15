9a7b957be7b21b6b01b1d44bada354cd37e800ac
MAwais <mawais54013@outlook.com>
2019-12-15 15:25:01 -0800

    Added Update

--- NewsFlash/NewsHelper.swift
+++ NewsFlash/NewsHelper.swift
@@ -18,6 +18,27 @@ class NewsHelper {
             case .success(let value):
                 if let json = value as? [String: Any] {
                     print(json)
+                    if let jsonArticles = json["articles"] as? [[String:Any]] {
+                        var articles = [Article]()
+                        for jsonArticle in jsonArticles {
+                            
+                            guard let title = jsonArticle["title"] as? String,
+                            let urlToImage = jsonArticle["urlToImage"] as? String,
+                            let url = jsonArticle["url"] as? String,
+                            let description = jsonArticle["description"] as? String
+                                
+                            else
+                            {
+                               continue
+                            }
+                            let article = Article()
+                            article.title = title
+                            article.urlToImage = urlToImage
+                            article.url = url
+                            article.description = description
+                            articles.append(article)
+                        }
+                    }
                 }
             case .failure(let error):
                 print(error)
